fc3440cf0a08bf6b700e05ff583951595075e659
KeshavJha <keshavmukundjha@gmail.com>
2022-08-01 06:31:01 +0530

    ReactJS learning code

---
+++ ReactJsProjects/ReactJsKeyConceptsBook/chap2_understanding_jsx/src/AppReplacement.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function AppReplacement() {
+    return (
+        <div >
+            <p className='App-link'>
+                Author : Keshav Mukund Jha
+            </p>
+        </div>
+    )
+}
+
+export default AppReplacement;
